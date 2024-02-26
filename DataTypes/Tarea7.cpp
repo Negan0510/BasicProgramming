214f3177acef0b4a2456b424477fc42d33b6f6eb
Negan0510 <160759977+Negan0510@users.noreply.github.com>
2024-02-26 16:13:34 -0500

    Add files via upload

---
+++ DataTypes/Tarea7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+  int main() {
+  int x = 20;
+  int y = 1000;
+  cout << x % y;
+  return 0;
+}
---
+++ DataTypes/Tarea9.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+  int main() {
+  int x = 1001;
+  --x;
+  cout << x;
+  return 0;
+}
+
