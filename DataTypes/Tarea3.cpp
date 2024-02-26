f527b4d67afa4df71b0abbfd4aec3d16521850b6
Negan0510 <160759977+Negan0510@users.noreply.github.com>
2024-02-26 15:50:31 -0500

    Add files via upload

---
+++ DataTypes/Tarea3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum1 = 177 + 33;        // 210 (177 + 33)
+  int sum2 = sum1 + 15;      // 400 (210 + 15)
+  int sum3 = sum2 + sum2;     // 610 (177 + 400)
+  cout << sum1 << "\n";
+  cout << sum2 << "\n";
+  cout << sum3;
+  return 0;
+}
